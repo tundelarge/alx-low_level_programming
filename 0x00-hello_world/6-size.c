6cdf6e895d6973a6fafbf69f6c6bcfcd2192543d
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2022-09-09 00:29:44 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -22,7 +22,7 @@ int main(void)
 	
 	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(c));
 	
-	printf("Size of a float: %lu byte(s)\n",(unsigned long) sizeof(f));
+	printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
 	
 	return (0);
 	
