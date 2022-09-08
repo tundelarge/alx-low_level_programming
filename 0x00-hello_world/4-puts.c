1a1c7aca6f9a1895b4dbc6f0c2af9297b703a8e4
tundelarge <talkabouttunde@yahoo.com>
2022-09-08 10:52:59 -0700

    Puts-lang

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main-Entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+
+	puts("\"Programing is like building a multilingual puzzle")
+
+		return (0);
+
+}
