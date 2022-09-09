b648a2b5bb26240f8410cfd2fc0b36661a538696
tundelarge <talkabouttunde@yahoo.com>
2022-09-09 16:40:01 -0700

    Print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include
+<sdio.h>
+/**
+ * main-entry point
+ *
+ * Return: alway return 0
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a ='a;a <=
+			'z';a++)
+
+		putchar(a);
+	putchar('n');
+	return (0);
+
+}
+
