887e1b75914927c366086c75bacd68c02c886742
tundelarge <talkabouttunde@yahoo.com>
2022-09-10 07:56:19 -0700

    Print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return always Return 0
+ *
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (; a < 10; a++)
+		printf("%d",a);
+	putchar('\n');
+	reurn (0);
+}
+
