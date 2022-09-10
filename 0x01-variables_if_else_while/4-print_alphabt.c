bba6c460cb27dc664a6321730a07927c18e9fdb6
tundelarge <talkabouttunde@yahoo.com>
2022-09-10 07:43:12 -0700

    Alphabt.P

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always Return 0
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'q' || a == 'e')
+		{
+			continue;
+		}
+		putchar (a);
+	}
+	putchar ('\n');
+	return (0);
+}
+
