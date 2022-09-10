421f20810e9671a4bfaf0f3d8f81cacba9af4711
tundelarge <talkabouttunde@yahoo.com>
2022-09-10 10:07:57 -0700

    Combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 10; i < 20; i++);
+	if (i != 19)
+	{
+		putchar((i % 10; i++);
+				if (i != 19)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+				}
+				putchar('\n');
+				return (0);
+				}
