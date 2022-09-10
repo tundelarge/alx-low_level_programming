5dee6357d8e796af8386277c01966b1b8ea323e4
tundelarge <talkabouttunde@yahoo.com>
2022-09-10 09:39:55 -0700

    Tebah

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return; always return 0
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a' ; a--)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
