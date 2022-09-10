a37753297c4d67ab72b8d0d8d70858b951f31b28
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2022-09-10 17:08:12 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,4 @@
-#include<stdlib.h>
-#include<time.h>
 #include<stdio.h>
-
 /**
  * main - Entry point
  *
@@ -15,6 +12,6 @@ int main(void)
 	for (; a < 10 ; a++)
 		putchar('0' + a);
 	putchar('\n');
-	reurn (0);
+	return (0);
 }
 
