702e8b7dbe09e2023a8e05ba0520931804193a89
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2022-09-10 19:54:57 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,32 +1,32 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
+ * main - entry point
  *
- * Return: always return 0
+ * Return: alway returns 0
  *
  */
 int main(void)
 {
 	int i;
-	int j:
+	int j;
 
-		for (i = 10; i <= 19; i++)
+	for (i = 10; i <= 19; i++)
+	{
+		for (j = 10; j <= 19; j++)
 		{
-			for (j = 10; j <= 19; j++)
+			if ((j % 10) > (i % 10))
 			{
-				if ((j % 10) > (i % 10))
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 18 || j != 19)
 				{
-					putchar((i % 10) + '0');
-					putchar((j % 10) + '0');
-					if (i != 18 || j != 19)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
-			
 			}
+
 		}
+	}
 	putchar('\n');
 	return (0);
 }
