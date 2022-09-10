eaaaf833ee1e89d93084043e75254f72d6b44fe7
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2022-09-10 20:09:12 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,32 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
 /**
  * main - entry point
  *
- * Return:always return 0
+ * Return: alway returns 0
  *
  */
 int main(void)
 {
-	int n;
+	int i;
+	int j;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n < 0)
+	for (i = 10; i <= 19; i++)
 	{
-		printf("%d is negative\n", n);
-	} else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	} else
-	{
-		printf("%d is positive\n", n);
+		for (j = 10; j <= 19; j++)
+		{
+			if ((j % 10) > (i % 10))
+			{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 18 || j != 19)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
 	}
+	putchar('\n');
 	return (0);
 }
