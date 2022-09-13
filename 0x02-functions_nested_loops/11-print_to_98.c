fb06ea04368006238cc0ebe74a3e2e0b43e0d911
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2022-09-13 22:00:13 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 			printf("%d", i);
 			if (i != 98)
 			{
-				printf(",");
+				printf(", ");
 			}
 		}
 	} else
@@ -23,7 +23,7 @@ void print_to_98(int n)
 			printf("%d", i);
 			if (i != 98)
 			{
-				printf(",");
+				printf(", ");
 			}
 		}
 	}
