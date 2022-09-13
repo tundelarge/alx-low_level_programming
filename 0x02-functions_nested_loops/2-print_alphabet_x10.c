88fa0d23acb2e8efa63ee69a14cf628367222e2e
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2022-09-13 21:33:17 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,3 +1,21 @@
-qwertyuiopasdfgh
-azxcvbn
-asdff
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: always 0;
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	char h;
+
+	for (h = 0; h <= 9; h++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
