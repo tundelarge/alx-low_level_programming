30e4fd1e3e21c5e77e36e065e16e896e33094519
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2022-09-16 00:30:36 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,3 +1,17 @@
-asdfghjkl
-zxcvbnm
-as
+#include "main.h"
+/**
+ * print_numbers - prints the numbers
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+Footer
+© 2022 GitHub, Inc.
