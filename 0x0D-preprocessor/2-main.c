f958ff820ff3d626267cac0b98d93223ceb2f468
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2022-10-10 16:20:38 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,12 @@
-qwertyu
+#include <stdio.h>
+/**
+ * main - Program compilation starts from the main
+ * Description: this program prints the name of the file it was comipiled from
+ * author: tundelarge
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
