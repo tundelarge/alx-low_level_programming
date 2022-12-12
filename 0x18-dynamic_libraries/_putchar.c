dc5f4025cd9bced3c2d9da4ad0df61d0dcd8b562
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2022-12-12 12:36:02 +0100

    Update _putchar.c

--- 0x18-dynamic_libraries/_putchar.c
+++ 0x18-dynamic_libraries/_putchar.c
@@ -1,1 +1,13 @@
-qwertyu
+#include <unistd.h>
+#include "main.h"
+/**
+ * _putchar - prints out a character to the stdout
+ * @c: character to be printed
+ * Return: an integer when
+ */
+int  _putchar(char c)
+{
+	write(1, &c, 1);
+
+	return (0);
+}
