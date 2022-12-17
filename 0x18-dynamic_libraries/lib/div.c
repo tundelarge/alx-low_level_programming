a87f4a5516effe1f390abcaf4e7ffc2fc32d5265
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2022-12-17 01:33:31 +0100

    Update div.c

--- 0x18-dynamic_libraries/lib/div.c
+++ 0x18-dynamic_libraries/lib/div.c
@@ -1,1 +1,11 @@
-qwerty
+#include "main.h"
+/**
+ * div - divides two integers
+ * @a: integer to be divided
+ * @b: integer to be divided
+ * Returns: returns the division of a and b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
