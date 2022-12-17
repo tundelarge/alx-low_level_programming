672b9f6f5b9421f56a180e38f5be397256c11ad0
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2022-12-17 01:36:25 +0100

    Update mul.c

--- 0x18-dynamic_libraries/lib/mul.c
+++ 0x18-dynamic_libraries/lib/mul.c
@@ -1,1 +1,11 @@
-qwerty
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: integer to be multiplied
+ * @b: integer to be multiplied
+ * Returns: returns the multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
