3b4aa7880cea4bd1f6d0a80aedfb39c04b4751b1
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2022-12-17 01:35:26 +0100

    Update mod.c

--- 0x18-dynamic_libraries/lib/mod.c
+++ 0x18-dynamic_libraries/lib/mod.c
@@ -1,1 +1,11 @@
-qwerty
+#include "main.h"
+/**
+ * mod - gets the remainder of two integers
+ * @a: integer
+ * @b: integer
+ * Returns: returns the remainder of (a / b)
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
