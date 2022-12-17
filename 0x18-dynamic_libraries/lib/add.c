b3fb2a1c2c6a933a4ba1d5ec76589751acbb8ddc
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2022-12-17 01:32:36 +0100

    Update add.c

--- 0x18-dynamic_libraries/lib/add.c
+++ 0x18-dynamic_libraries/lib/add.c
@@ -1,1 +1,11 @@
-qwerty
+#include "main.h"
+/**
+ * add - adds two integers
+ * @a: integer to be added
+ * @b: integer to be added
+ * Returns: returns the addition of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
